68621e5b74977b89ace86a19d3655361fb78a4f8
玲珑飞梦—星辰大海 <108404516+hjsxhst2022@users.noreply.github.com>
2023-06-18 09:18:22 +0800

    Add files via upload

---
+++ data.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	system("del D:\\answer.data");
+	fstream r1;
+	r1.open("D:\\ask.data",ios::out);
+	r1<<"1+1\nyour name\nhow are you";
+	r1.close();
+}
